821566fc09b170bb07b5000bff7c5c17bc91ef56
lhamowski <lukaszhamowski@gmail.com>
2025-07-04 17:47:55 +0200

    Add finally_guard with unit tests

---
+++ include/lux/support/finally.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <functional>
+#include <utility>
+
+namespace lux {
+namespace detail {
+
+template <typename F>
+class finally_guard
+{
+public:
+    explicit finally_guard(F&& f) : func_(std::forward<F>(f)) {}
+
+    ~finally_guard() { func_(); }
+
+    finally_guard(const finally_guard&) = delete;
+    finally_guard& operator=(const finally_guard&) = delete;
+    finally_guard(finally_guard&&) = delete;
+    finally_guard& operator=(finally_guard&&) = delete;
+
+private:
+    F func_;
+};
+
+template <typename F>
+finally_guard<F> make_finally(F&& f)
+{
+    return finally_guard<F>(std::forward<F>(f));
+}
+
+} // namespace detail
+} // namespace lux
+
+#define LUX_CONCAT_IMPL(x, y) x##y
+#define LUX_CONCAT(x, y) LUX_CONCAT_IMPL(x, y)
+
+#define LUX_FINALLY(code) auto LUX_CONCAT(finally_, __LINE__) = ::lux::detail::make_finally([&]() { code; })
---
+++ test/finally_test.cpp
@@ -0,0 +1,36 @@
+#include <lux/support/finally.hpp>
+#include <catch2/catch_all.hpp>
+#include <filesystem>
+#include <fstream>
+
+TEST_CASE("Finally macro basic functionality", "[finally]")
+{
+    SECTION("Finally executes code at scope exit")
+    {
+        bool executed = false;
+
+        {
+            LUX_FINALLY(executed = true);
+            CHECK_FALSE(executed); // Should not be executed yet
+        }
+
+        CHECK(executed); // Should be executed after scope exit
+    }
+
+    SECTION("Finally executes even when exception is thrown")
+    {
+        bool cleanup_executed = false;
+
+        try
+        {
+            LUX_FINALLY({cleanup_executed = true;});
+            throw std::runtime_error("test exception");
+        }
+        catch (const std::exception&)
+        {
+            // Exception caught
+        }
+
+        CHECK(cleanup_executed);
+    }
+}
