998c4bfaaab4fa6c8e41f5480ab302a495539892
lhamowski <lukaszhamowski@gmail.com>
2025-10-26 13:50:14 +0100

    Add missing forward declarations

--- include/lux/fwd.hpp
+++ include/lux/fwd.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 namespace lux {
+
 class logger_factory;
 class logger_manager;
 class logger;
@@ -9,6 +10,10 @@ class memory_arena;
 } // namespace lux
 
 namespace lux::net::base {
+
+class endpoint;
+class host_endpoint;
+
 class socket_factory;
 
 class tcp_acceptor;
@@ -19,20 +24,31 @@ class tcp_socket_handler;
 
 class udp_socket;
 class udp_socket_handler;
+
+class websocket;
+class websocket_handler;
+
 } // namespace lux::net::base
 
 namespace lux::net {
-class udp_socket;
+
 class socket_factory;
+class tcp_socket;
+class udp_socket;
+
 } // namespace lux::net
 
 namespace lux::time::base {
+
 class timer_factory;
 class interval_timer;
+
 } // namespace lux::time::base
 
 namespace lux::time {
+
 struct delayed_retry_config;
 
 class delayed_retry_executor;
+
 } // namespace lux::time
